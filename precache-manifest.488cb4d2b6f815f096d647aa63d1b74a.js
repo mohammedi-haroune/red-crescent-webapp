af15c2eec27e2e5ba4357b1b44cc92676989484c
MOHAMMEDI Haroune <mohammedi.haroun@gmail.com>
2019-11-21 21:12:12 +0100

    Updates

---
+++ precache-manifest.488cb4d2b6f815f096d647aa63d1b74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8a58d6d155f459f951f7577ab65c2d",
+    "url": "/red-crescent-webapp/index.html"
+  },
+  {
+    "revision": "85c8f140bd1b357a8f69",
+    "url": "/red-crescent-webapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1e46bcfdfd7968c579be",
+    "url": "/red-crescent-webapp/static/js/2.89ccdacd.chunk.js"
+  },
+  {
+    "revision": "85c8f140bd1b357a8f69",
+    "url": "/red-crescent-webapp/static/js/main.6455de22.chunk.js"
+  },
+  {
+    "revision": "37553ba581196bc2e9e4",
+    "url": "/red-crescent-webapp/static/js/runtime-main.bc67fe85.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/red-crescent-webapp/static/media/logo.25bf045c.svg"
+  }
+]);
