ee65a1e7b2cc0d36aa8ddba891500057a4a2b60a
DallelG <gaham.d@hotmail.fr>
2019-11-25 20:58:41 +0100

    Feat: Display hospitals locations on a map
    create a map which displays the hospitals data

--- src/components/HospitalsMap.js
+++ src/components/HospitalsMap.js
@@ -1,54 +1,38 @@
-import React, {Component, Fragment} from 'react'
-import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import React, {Component} from 'react'
+import {Map, TileLayer, GeoJSON, Marker, Popup} from 'react-leaflet'
 import algiers_hospitals from '../data/hospitals.json'
-import hash from 'object-hash'
 import './HospitalsMap.css'
 type State = {
   lat: number,
   lng: number,
   zoom: number,
-  markers: Array<MarkerData>
 }
-
-type Position = [number, number]
-
-type Props = {|content: string, position: Position,|}
-
-type MarkerData = {| ...Props, key:string|}
-
-const MyPopupMarker = ({content, position}: Props)=>(
-  <Marker position={position}>
-    <Popup>{content}</Popup>
-  </Marker>
-  )
-
-  const MyMarkersList = ({markers}:{markers : Array<MarkerData>}) =>{
-    const items = markers.map(({key, ...props}) =>
-    { return <MyPopupMarker key={hash(content)} {...props} />
-    })
-    return <Fragment>{items}</Fragment>
-  }
 
 export default class HospitalsMap extends Component<{}, State> {
   state = {
     lat: 36.7628,
     lng: 3.0531,
     zoom: 12,
-    markers: algiers_hospitals
   }
 
   render(){
-    const center = [this.state.lat, this.state.lng]
-    return (
-      <Map center={center} zoom={this.state.zoom} id="map">
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      <MyMarkersList  markers={this.state.markers}/>
+    const position = [this.state.lat, this.state.lng];
+    let markers = algiers_hospitals;
 
-      </Map>
+       return (
+         <Map center={position} zoom={this.state.zoom} id="map">
+           <TileLayer
+             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+           />
 
-    )
+
+         {markers.map((value,index)=>{
+           return <Marker position={value.geometry.coordinates} > <Popup><h3>{value.properties.name}</h3></Popup> </Marker>
+             })}
+
+         </Map>
+       )
+
   }
 }
