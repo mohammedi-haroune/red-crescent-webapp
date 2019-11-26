668e2be2b7a4e7ae238030f7173d829de60ea166
Mehdi Messarat <mehdimessarat@outlook.fr>
2019-11-26 18:17:51 +0100

    Feat[Maps]: Showing in Maps hospitals
    Adding the feature where we can see in the map, the title of hospitals

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import {Hospitals} from './components';
 import './App.css';
 import {hospitals} from './data.js';
+import HospitalsMap from './components/HospitalsMap'
+import L from 'leaflet';
 
 function App() {
+  React.useEffect(() => {
+
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1 style={{textAlign:'center'}}> HOPITAUX </h1>
       <Hospitals hospitals={hospitals} />
+      <HospitalsMap />
     </div>
   );
 }
