0747ac6ca803802ae1571e6a8c56531d34d0d81f
Mehdi Messarat <mehdimessarat@outlook.fr>
2019-11-29 15:54:50 +0100

    Feat[Contact]: Adding Router
    Adding Router so that we can route to the different views, including
    Contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {LandingPage, Contact} from './views';
 import './App.css';
+import { createBrowserHistory } from "history";
+import { Router, Route, Redirect, Switch } from "react-router-dom";
+var hist = createBrowserHistory();
 
 function App() {
   return (
+    <Router history={hist}>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+        <Route path="/Contact" component={Contact} />
+        <Route path="/hopitaux" component={LandingPage} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
